5dbb9fdf9aa5a299277a7cab06ac6d024c416d28
Victor Mamede <victoraugustomamede@hotmail.com>
2023-04-28 03:10:16 -0300

    small fixes

--- front/src/pages/Default.tsx
+++ front/src/pages/Default.tsx
@@ -1,28 +1,30 @@
-import { Center, Grid, GridItem } from "@chakra-ui/react";
+import { Center, Grid, GridItem, Text } from "@chakra-ui/react";
 import Station from "../components/Station";
 import trpc from "../util/trpc";
 
 export default function Default() {
-  const { data, refetch } = trpc.connections.useQuery();
+  const { data, refetch, isLoading } = trpc.connections.useQuery();
   trpc.onConnectionChanged.useSubscription(undefined, {
     onData() {
       refetch();
     },
   });
 
-  if (data == null) {
-    return <>No data</>;
-  }
-
   return (
     <Center h="100vh">
-      <Grid templateColumns="repeat(2, 1fr)" gap={5}>
-        {Object.entries(data).map(([key, value]) => (
-          <GridItem key={key}>
-            <Station name={key} info={value || undefined} />
-          </GridItem>
-        ))}
-      </Grid>
+      {data ? (
+        <Grid templateColumns="repeat(2, 1fr)" gap={5}>
+          {Object.entries(data).map(([key, value]) => (
+            <GridItem key={key}>
+              <Station name={key} info={value || undefined} />
+            </GridItem>
+          ))}
+        </Grid>
+      ) : isLoading ? (
+        <Text>Carregando...</Text>
+      ) : (
+        <Text>Nada a exibir</Text>
+      )}
     </Center>
   );
 }
